f9d03a3a18393ca30070756444d50848f7aa8a29
rodrex25 <gabresh25@gmail.com>
2025-08-18 16:58:07 -0600

    first commit

---
+++ AsyncProgNet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Start simulate delay");
+           SimulateDelay().GetAwaiter().GetResult();
+
+        }
+
+        public static async Task SimulateDelay()
+        {
+            await Task.Delay(5000);
+            System.Console.WriteLine("Operation completed after a delay");
+        }
+    }
+}
